832cf44eea359bb41f284813c22e64bd6e4e00de
Serkan Çakmak <cakmakserkan07@gmail.com>
2025-01-14 12:39:37 +0300

    Takip edilmeyen dosyalar hariç tutuldu

--- inventory/inventory_track/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InventoryTrackConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventory_track'
--- inventory/inventory_track/templatetags/nav_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.simple_tag(takes_context=True)
-def active_class(context, url_name):
-    request = context['request']
-    return 'active' if request.resolver_match.url_name == url_name else ''
